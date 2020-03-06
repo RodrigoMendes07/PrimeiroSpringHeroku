5578ff59b442774c7cd1ecfdc5fb84061e42a2e3
Ti <Ti@DESKTOP-7BPSDVM>
2020-03-06 17:11:51 -0400

    Atualização 06/03/2020

--- primeroapp/src/main/java/com/testeapp/testeapp/TesteappApplication.java
+++ primeroapp/src/main/java/com/testeapp/testeapp/TesteappApplication.java
@@ -2,11 +2,19 @@ package com.testeapp.testeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class TesteappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteappApplication.class, args);
+	}
+	
+	@RequestMapping("/home")
+	public String index(){
+		return "index";
 	}
 }
