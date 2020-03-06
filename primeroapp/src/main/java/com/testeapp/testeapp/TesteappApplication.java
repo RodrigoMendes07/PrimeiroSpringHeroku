8abd0474df1bd2db4c97c12a92537ca74abb26ea
Ti <Ti@DESKTOP-7BPSDVM>
2020-03-06 16:30:42 -0400

    PrimeiraHeroku up version 0.0

---
+++ primeroapp/src/main/java/com/testeapp/testeapp/TesteappApplication.java
@@ -0,0 +1,12 @@
+package com.testeapp.testeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteappApplication.class, args);
+	}
+}
---
+++ primeroapp/src/test/java/com/testeapp/testeapp/TesteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeapp.testeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
